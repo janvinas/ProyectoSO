d4e08e6790e2f8466108d394cde3b08ca5e2472c
Jan Viñas Corella <mirabetjan@gmail.com>
2023-10-14 19:51:13 +0200

    añadir concurrencia

--- Cliente/ProyectoSO/Login/Form1.Designer.cs
+++ Cliente/ProyectoSO/Login/Form1.Designer.cs
@@ -39,9 +39,10 @@
             // 
             // Conectar
             // 
-            this.Conectar.Location = new System.Drawing.Point(362, 61);
+            this.Conectar.Location = new System.Drawing.Point(241, 40);
+            this.Conectar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Conectar.Name = "Conectar";
-            this.Conectar.Size = new System.Drawing.Size(169, 46);
+            this.Conectar.Size = new System.Drawing.Size(113, 30);
             this.Conectar.TabIndex = 0;
             this.Conectar.Text = "Conectarse";
             this.Conectar.UseVisualStyleBackColor = true;
@@ -50,9 +51,10 @@
             // IP
             // 
             this.IP.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.IP.Location = new System.Drawing.Point(130, 75);
+            this.IP.Location = new System.Drawing.Point(87, 49);
+            this.IP.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.IP.Name = "IP";
-            this.IP.Size = new System.Drawing.Size(193, 32);
+            this.IP.Size = new System.Drawing.Size(130, 24);
             this.IP.TabIndex = 1;
             this.IP.Text = "192.168.56.102";
             // 
@@ -60,17 +62,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(54, 62);
+            this.label1.Location = new System.Drawing.Point(36, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 46);
+            this.label1.Size = new System.Drawing.Size(40, 31);
             this.label1.TabIndex = 2;
             this.label1.Text = "IP";
             // 
             // desconectar
             // 
-            this.desconectar.Location = new System.Drawing.Point(594, 62);
+            this.desconectar.Location = new System.Drawing.Point(396, 40);
+            this.desconectar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.desconectar.Name = "desconectar";
-            this.desconectar.Size = new System.Drawing.Size(169, 46);
+            this.desconectar.Size = new System.Drawing.Size(113, 30);
             this.desconectar.TabIndex = 3;
             this.desconectar.Text = "Desconectarse";
             this.desconectar.UseVisualStyleBackColor = true;
@@ -78,9 +82,10 @@
             // 
             // login
             // 
-            this.login.Location = new System.Drawing.Point(152, 271);
+            this.login.Location = new System.Drawing.Point(101, 176);
+            this.login.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.login.Name = "login";
-            this.login.Size = new System.Drawing.Size(137, 48);
+            this.login.Size = new System.Drawing.Size(91, 31);
             this.login.TabIndex = 4;
             this.login.Text = "Login";
             this.login.UseVisualStyleBackColor = true;
@@ -88,9 +93,10 @@
             // 
             // signup
             // 
-            this.signup.Location = new System.Drawing.Point(394, 271);
+            this.signup.Location = new System.Drawing.Point(263, 176);
+            this.signup.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.signup.Name = "signup";
-            this.signup.Size = new System.Drawing.Size(137, 48);
+            this.signup.Size = new System.Drawing.Size(91, 31);
             this.signup.TabIndex = 5;
             this.signup.Text = "Signup";
             this.signup.UseVisualStyleBackColor = true;
@@ -99,9 +105,10 @@
             // consultasBasicas
             // 
             this.consultasBasicas.Enabled = false;
-            this.consultasBasicas.Location = new System.Drawing.Point(610, 271);
+            this.consultasBasicas.Location = new System.Drawing.Point(407, 176);
+            this.consultasBasicas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.consultasBasicas.Name = "consultasBasicas";
-            this.consultasBasicas.Size = new System.Drawing.Size(174, 48);
+            this.consultasBasicas.Size = new System.Drawing.Size(116, 31);
             this.consultasBasicas.TabIndex = 6;
             this.consultasBasicas.Text = "Consultas Básicas";
             this.consultasBasicas.UseVisualStyleBackColor = true;
@@ -109,9 +116,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(901, 511);
+            this.ClientSize = new System.Drawing.Size(601, 332);
             this.Controls.Add(this.consultasBasicas);
             this.Controls.Add(this.signup);
             this.Controls.Add(this.login);
@@ -119,8 +126,10 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.IP);
             this.Controls.Add(this.Conectar);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
--- Servidor/servidor.c
+++ Servidor/servidor.c
@@ -6,11 +6,192 @@
 #include <netinet/in.h>
 #include <mysql.h>
 #include <stdio.h>
+#include <pthread.h>
+
+MYSQL *conn;
+MYSQL_RES *resultado;
+MYSQL_ROW row;
+
+void login(char *response){
+	char nombre[40];
+	strcpy(nombre, strtok(NULL, "/"));
+	char password[40];
+	strcpy(password, strtok(NULL, "/"));
+	char query[500];
+	sprintf(query, "SELECT Usuario FROM Jugador WHERE Usuario='%s' AND Password='%s'", nombre, password);
+	int result = mysql_query(conn, query);
+	if(result != 0){
+		sprintf(response, "-1");
+	}
+	else{
+		resultado = mysql_store_result (conn);
+		row = mysql_fetch_row (resultado);
+		if (row == NULL)
+			sprintf(response,"0");
+		else
+			sprintf(response,"1");
+	}
+}
+
+void *atenderCliente(void *socket){
+	int sock_conn = *((int*) socket);
+
+	char buff[512];
+	char buff2[512];
+
+	while(1){
+		int ret=read(sock_conn,buff, sizeof(buff));
+		//posa un valor 0 al final per acabar la string
+		buff[ret]='\0';
+
+		printf("Mensaje recibido!: %s\n", buff);
+
+		char *token = strtok(buff, "/");
+		int codigo = atoi(token);
+
+		if(codigo == 0){
+			close(sock_conn);
+			printf("Cliente desconectado\n");
+			break;
+		}
+		else if (codigo ==1){
+			login(buff2);
+		}
+		else if(codigo == 2){
+			char nombre[50];
+			strcpy(nombre, strtok(NULL, "/"));
+			char password[50];
+			strcpy(password, strtok(NULL, "/"));
+			char email[90];
+			strcpy(email, strtok(NULL, "/"));
+			char genero = strtok(NULL, "/")[0];	//el género tiene un único carácter
+			
+			
+			char query1[500];
+			sprintf(query1, "SELECT Usuario FROM Jugador WHERE Usuario='%s'", nombre);
+			int result = mysql_query(conn, query1);
+			if(result != 0){
+				sprintf(buff2, "-1");
+			}
+			else{
+				resultado = mysql_store_result (conn);
+				row = mysql_fetch_row (resultado);
+				if (row == NULL){
+					char query2[500];
+					sprintf(query2, "INSERT INTO Jugador (Usuario, Password, Mail, Genero) VALUES ('%s', '%s', '%s', '%c');", nombre, password, email, genero);
+					result = mysql_query(conn, query2);
+					printf("%s\n",query2);
+					if(result != 0){
+						sprintf(buff2, "-1");
+					}else{
+						sprintf(buff2, "1");
+					}
+				}else{
+					sprintf(buff2,"0"); //Usuario o contraseña existentes
+				}
+			}
+			
+		}else if(codigo == 3){
+			char nombre[40];
+			strcpy(nombre, strtok(NULL, "/"));
+			char query[500];
+			sprintf(query, "SELECT Usuario FROM Jugador WHERE Usuario='%s'", nombre);
+			int result = mysql_query(conn, query);
+			if(result != 0){
+				sprintf(buff2, "-1");
+			}
+			else{
+				resultado = mysql_store_result (conn);
+				row = mysql_fetch_row (resultado);
+				if (row == NULL)
+					sprintf(buff2,"0");
+				else
+					sprintf(buff2,"1");
+			}
+		}else if(codigo == 4){
+			char construccion[50];
+			strcpy(construccion, strtok(NULL, "/"));
+			char query[500];
+			sprintf(query, "SELECT Partidas.ID, Part_Const.X, Part_Const.Y FROM (Construcciones, Partidas, Part_Const) "
+					"WHERE Construcciones.nombre = '%s' "
+					"AND Construcciones.ID = Part_Const.ID_Const "
+					"AND Partidas.ID = Part_Const.ID_Part;", construccion);
+			int err = mysql_query(conn, query);
+			if(err != 0){
+				sprintf(buff2, "-1");
+			}else{
+				resultado = mysql_store_result (conn);
+				row = mysql_fetch_row(resultado);
+				if(row == NULL){
+					sprintf(buff2, "0");
+				}else{
+					sprintf(buff2, "%s/%s/%s", row[0], row[1], row[2]);
+					row = mysql_fetch_row (resultado);
+					while(row != NULL){
+						sprintf(buff2, "%s/%s/%s/%s", buff2, row[0], row[1], row[2]);
+						row = mysql_fetch_row (resultado);
+					}
+					printf("%s\n", buff2);
+				}
+			}
+			
+		}
+		else if(codigo==5){
+			char nombre[80];
+			token=strtok(NULL,"/");
+			strcpy(nombre,token);
+			char query1[500];
+			sprintf(query1, "SELECT Jug_Part.Dinero FROM (Jugador, Partidas,Jug_Part) WHERE Jugador.Usuario = '%s' AND Jugador.ID = Jug_Part.Jugador AND Partidas.ID = Jug_Part.Partida;", nombre);
+			int result = mysql_query(conn, query1);
+			if(result != 0){
+				sprintf(buff2, "-1");
+			}
+			else{
+				resultado = mysql_store_result (conn);
+				row = mysql_fetch_row (resultado);
+				if (row == NULL)
+					sprintf(buff2,"-2");
+				else{
+					int dtotal=0;
+					while(row!=NULL){
+						dtotal += atoi(row[0]);
+						row = mysql_fetch_row (resultado);
+					}
+					sprintf(buff2,"%d",dtotal);
+				}
+			}
+		}
+		else if(codigo==6){
+			char query[500];
+			sprintf(query, "SELECT Jugador.Usuario, Logros.descripcion FROM (Jugador, Logros, Jug_Log) WHERE Jug_Log.logro_obtenido = 'yes' AND Jug_Log.recompensa_obtenida = 'no' AND Jugador.ID = Jug_Log.ID_Jugadores AND Logros.ID = Jug_Log.ID_Logro;");
+			int err = mysql_query(conn, query);
+			if(err != 0){
+				sprintf(buff2, "-1");
+			}else{
+				resultado = mysql_store_result (conn);
+				row = mysql_fetch_row(resultado);
+				if(row == NULL){
+					sprintf(buff2, "0");
+				}else{
+					sprintf(buff2, "%s/%s", row[0], row[1]);
+					row = mysql_fetch_row (resultado);
+					while(row != NULL){
+						sprintf(buff2, "%s/%s/%s", buff2, row[0], row[1]);
+						row = mysql_fetch_row (resultado);
+					}
+					printf("%s\n", buff2);
+				}
+			}
+		}
+
+		//imprimeix el buffer al socket i tanca'l
+		//la resposta només s'envia si el codi del missatge no és 0
+		write(sock_conn,buff2, strlen(buff2));
+		printf("Respuesta enviada!: %s\n", buff2);
+	}
+}
 
 int main(int argc, char *charv[]){
-	MYSQL *conn;
-	MYSQL_RES *resultado;
-	MYSQL_ROW row;
 	conn = mysql_init(NULL);
 	if(conn==NULL){
 		printf("Error creant connexió SQL: %u %s\n", mysql_errno(conn), mysql_error(conn));
@@ -22,11 +203,9 @@ int main(int argc, char *charv[]){
 		exit(-1);
 	}
 
-	int sock_conn, sock_listen, ret;
+	int sock_listen;
 	struct sockaddr_in serv_adr;
-	char buff[512];
-	char buff2[512];
-
+	
 	if((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 		printf("Error creant socket");
 		exit(-1);
@@ -47,161 +226,15 @@ int main(int argc, char *charv[]){
 		exit(-1);
 	}
 
-	// bucle infinit per rebre peticions dels clients
+	int sockets[100];
+	int i = 0;
 	while(1){
 		printf("Escuchando\n");
-		//espera fins que un client realitzi una connexió
-		sock_conn = accept(sock_listen, NULL, NULL);
+		int sock_conn = accept(sock_listen, NULL, NULL);
 		printf("Cliente conectado\n");
-		while(1){
-			ret=read(sock_conn,buff, sizeof(buff));
-			//posa un valor 0 al final per acabar la string
-			buff[ret]='\0';
-
-			printf("Mensaje recibido!: %s\n", buff);
-
-			char *token = strtok(buff, "/");
-			int codigo = atoi(token);
-
-			if(codigo == 0){
-				close(sock_conn);
-				printf("Cliente desconectado\n");
-				break;
-			}
-			else if (codigo ==1){
-				char nombre[40];
-				strcpy(nombre, strtok(NULL, "/"));
-				char password[40];
-				strcpy(password, strtok(NULL, "/"));
-				char query[500];
-				sprintf(query, "SELECT Usuario FROM Jugador WHERE Usuario='%s' AND Password='%s'", nombre, password);
-				int result = mysql_query(conn, query);
-				if(result != 0){
-					sprintf(buff2, "-1");
-				}
-				else{
-					resultado = mysql_store_result (conn);
-					row = mysql_fetch_row (resultado);
-					if (row == NULL)
-						sprintf(buff2,"0");
-					else
-						sprintf(buff2,"1");
-				}
-			}
-			else if(codigo == 2){
-				char nombre[50];
-				strcpy(nombre, strtok(NULL, "/"));
-				char password[50];
-				strcpy(password, strtok(NULL, "/"));
-				char email[90];
-				strcpy(email, strtok(NULL, "/"));
-				char genero = strtok(NULL, "/")[0];	//el género tiene un único carácter
-				
-				
-				char query1[500];
-				sprintf(query1, "SELECT Usuario FROM Jugador WHERE Usuario='%s'", nombre);
-				int result = mysql_query(conn, query1);
-				if(result != 0){
-					sprintf(buff2, "-1");
-				}
-				else{
-					resultado = mysql_store_result (conn);
-					row = mysql_fetch_row (resultado);
-					if (row == NULL){
-						char query2[500];
-						sprintf(query2, "INSERT INTO Jugador (Usuario, Password, Mail, Genero) VALUES ('%s', '%s', '%s', '%c');", nombre, password, email, genero);
-						result = mysql_query(conn, query2);
-						printf("%s\n",query2);
-						if(result != 0){
-							sprintf(buff2, "-1");
-						}else{
-							sprintf(buff2, "1");
-						}
-					}else{
-						sprintf(buff2,"0"); //Usuario o contraseña existentes
-					}
-				}
-				
-			}else if(codigo == 4){
-				char construccion[50];
-				strcpy(construccion, strtok(NULL, "/"));
-				char query[500];
-				sprintf(query, "SELECT Partidas.ID, Part_Const.X, Part_Const.Y FROM (Construcciones, Partidas, Part_Const) "
-					 "WHERE Construcciones.nombre = '%s' "
-					 "AND Construcciones.ID = Part_Const.ID_Const "
-					 "AND Partidas.ID = Part_Const.ID_Part;", construccion);
-				int err = mysql_query(conn, query);
-				if(err != 0){
-					sprintf(buff2, "-1");
-				}else{
-					resultado = mysql_store_result (conn);
-					row = mysql_fetch_row(resultado);
-					if(row == NULL){
-						sprintf(buff2, "0");
-					}else{
-						sprintf(buff2, "%s/%s/%s", row[0], row[1], row[2]);
-						row = mysql_fetch_row (resultado);
-						while(row != NULL){
-							sprintf(buff2, "%s/%s/%s/%s", buff2, row[0], row[1], row[2]);
-							row = mysql_fetch_row (resultado);
-						}
-						printf("%s\n", buff2);
-					}
-				}
-				
-			}
-			else if(codigo==5){
-				char nombre[80];
-				token=strtok(NULL,"/");
-				strcpy(nombre,token);
-				char query1[500];
-				sprintf(query1, "SELECT Jug_Part.Dinero FROM (Jugador, Partidas,Jug_Part) WHERE Jugador.Usuario = '%s' AND Jugador.ID = Jug_Part.Jugador AND Partidas.ID = Jug_Part.Partida;", nombre);
-				int result = mysql_query(conn, query1);
-				if(result != 0){
-					sprintf(buff2, "-1");
-				}
-				else{
-					resultado = mysql_store_result (conn);
-					row = mysql_fetch_row (resultado);
-					if (row == NULL)
-						sprintf(buff2,"-2");
-					else{
-						int dtotal=0;
-						while(row!=NULL){
-							dtotal += atoi(row[0]);
-							row = mysql_fetch_row (resultado);
-						}
-						sprintf(buff2,"%d",dtotal);
-					}
-				}
-			}
-			else if(codigo==6){
-				char query[500];
-				sprintf(query, "SELECT Jugador.Usuario, Logros.descripcion FROM (Jugador, Logros, Jug_Log) WHERE Jug_Log.logro_obtenido = 'yes' AND Jug_Log.recompensa_obtenida = 'no' AND Jugador.ID = Jug_Log.ID_Jugadores AND Logros.ID = Jug_Log.ID_Logro;");
-				int err = mysql_query(conn, query);
-				if(err != 0){
-					sprintf(buff2, "-1");
-				}else{
-					resultado = mysql_store_result (conn);
-					row = mysql_fetch_row(resultado);
-					if(row == NULL){
-						sprintf(buff2, "0");
-					}else{
-						sprintf(buff2, "%s/%s", row[0], row[1]);
-						row = mysql_fetch_row (resultado);
-						while(row != NULL){
-							sprintf(buff2, "%s/%s/%s", buff2, row[0], row[1]);
-							row = mysql_fetch_row (resultado);
-						}
-						printf("%s\n", buff2);
-					}
-				}
-			}
-
-
-			//imprimeix el buffer al socket i tanca'l
-			//la resposta només s'envia si el codi del missatge no és 0
-			write(sock_conn,buff2, strlen(buff2));
-		}
+		sockets[i] = sock_conn;
+		pthread_t t;	//no necessitem aquest valor, no el guardem enlloc
+		pthread_create(&t, NULL, atenderCliente, &sockets[i]);
+		i++;
 	}
 }
