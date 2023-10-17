fad4f101b5b405785dd6c139aa801da6f05f7331
Jan Viñas Corella <mirabetjan@gmail.com>
2023-10-17 20:58:50 +0200

    posar peticions en funcions

--- Servidor/servidor.c
+++ Servidor/servidor.c
@@ -33,6 +33,140 @@ void login(char *response){
 	}
 }
 
+void signup(char *response){
+	char nombre[50];
+	strcpy(nombre, strtok(NULL, "/"));
+	char password[50];
+	strcpy(password, strtok(NULL, "/"));
+	char email[90];
+	strcpy(email, strtok(NULL, "/"));
+	char genero = strtok(NULL, "/")[0];	//el género tiene un único carácter
+	
+	
+	char query1[500];
+	sprintf(query1, "SELECT Usuario FROM Jugador WHERE Usuario='%s'", nombre);
+	int result = mysql_query(conn, query1);
+	if(result != 0){
+		sprintf(response, "-1");
+	}
+	else{
+		resultado = mysql_store_result (conn);
+		row = mysql_fetch_row (resultado);
+		if (row == NULL){
+			char query2[500];
+			sprintf(query2, "INSERT INTO Jugador (Usuario, Password, Mail, Genero) VALUES ('%s', '%s', '%s', '%c');", nombre, password, email, genero);
+			result = mysql_query(conn, query2);
+			printf("%s\n",query2);
+			if(result != 0){
+				sprintf(response, "-1");
+			}else{
+				sprintf(response, "1");
+			}
+		}else{
+			sprintf(response,"0"); //Usuario o contraseña existentes
+		}
+	}
+}
+
+void existeUsuario(char *response){
+	char nombre[40];
+	strcpy(nombre, strtok(NULL, "/"));
+	char query[500];
+	sprintf(query, "SELECT Usuario FROM Jugador WHERE Usuario='%s'", nombre);
+	int result = mysql_query(conn, query);
+	if(result != 0){
+		sprintf(response, "-1");
+	}
+	else{
+		resultado = mysql_store_result (conn);
+		row = mysql_fetch_row (resultado);
+		if (row == NULL)
+			sprintf(response,"0");
+		else
+			sprintf(response,"1");
+	}
+}
+
+void consultaBasicaConstrucciones(char *response){
+	char construccion[50];
+	strcpy(construccion, strtok(NULL, "/"));
+	char query[500];
+	sprintf(query, "SELECT Partidas.ID, Part_Const.X, Part_Const.Y FROM (Construcciones, Partidas, Part_Const) "
+			"WHERE Construcciones.nombre = '%s' "
+			"AND Construcciones.ID = Part_Const.ID_Const "
+			"AND Partidas.ID = Part_Const.ID_Part;", construccion);
+	int err = mysql_query(conn, query);
+	if(err != 0){
+		sprintf(response, "-1");
+	}else{
+		resultado = mysql_store_result (conn);
+		row = mysql_fetch_row(resultado);
+		if(row == NULL){
+			sprintf(response, "0");
+		}else{
+			sprintf(response, "%s/%s/%s", row[0], row[1], row[2]);
+			row = mysql_fetch_row (resultado);
+			while(row != NULL){
+				sprintf(response, "%s/%s/%s/%s", response, row[0], row[1], row[2]);
+				row = mysql_fetch_row (resultado);
+			}
+			printf("%s\n", response);
+		}
+	}
+}
+
+void consultaBasicaDinero(char *response){
+	char nombre[80];
+	char *token=strtok(NULL,"/");
+	strcpy(nombre,token);
+	char query1[500];
+	sprintf(query1, "SELECT Jug_Part.Dinero FROM (Jugador, Partidas,Jug_Part) WHERE Jugador.Usuario = '%s' AND Jugador.ID = Jug_Part.Jugador AND Partidas.ID = Jug_Part.Partida;", nombre);
+	int result = mysql_query(conn, query1);
+	if(result != 0){
+		sprintf(response, "-1");
+	}
+	else{
+		resultado = mysql_store_result (conn);
+		row = mysql_fetch_row (resultado);
+		if (row == NULL)
+			sprintf(response,"-2");
+		else{
+			int dtotal=0;
+			while(row!=NULL){
+				dtotal += atoi(row[0]);
+				row = mysql_fetch_row (resultado);
+			}
+			sprintf(response,"%d",dtotal);
+		}
+	}
+}
+
+void consultaBasicaLogros(char *response){
+	char query[500];
+	sprintf(query, "SELECT Jugador.Usuario, Logros.descripcion FROM (Jugador, Logros, Jug_Log) WHERE Jug_Log.logro_obtenido = 'yes' AND Jug_Log.recompensa_obtenida = 'no' AND Jugador.ID = Jug_Log.ID_Jugadores AND Logros.ID = Jug_Log.ID_Logro;");
+	int err = mysql_query(conn, query);
+	if(err != 0){
+		sprintf(response, "-1");
+	}else{
+		resultado = mysql_store_result (conn);
+		row = mysql_fetch_row(resultado);
+		if(row == NULL){
+			sprintf(response, "0");
+		}else{
+			sprintf(response, "%s/%s", row[0], row[1]);
+			row = mysql_fetch_row (resultado);
+			while(row != NULL){
+				sprintf(response, "%s/%s/%s", response, row[0], row[1]);
+				row = mysql_fetch_row (resultado);
+			}
+			printf("%s\n", response);
+		}
+	}
+}
+
+
+// función que se ejecutará cada vez que un cliente se conecte y se cree un socket.
+// la función termina cuando el cliente se desconecta.
 void *atenderCliente(void *socket){
 	int sock_conn = *((int*) socket);
 
@@ -53,135 +187,18 @@ void *atenderCliente(void *socket){
 			close(sock_conn);
 			printf("Cliente desconectado\n");
 			break;
-		}
-		else if (codigo ==1){
+		}else if (codigo ==1){
 			login(buff2);
-		}
-		else if(codigo == 2){
-			char nombre[50];
-			strcpy(nombre, strtok(NULL, "/"));
-			char password[50];
-			strcpy(password, strtok(NULL, "/"));
-			char email[90];
-			strcpy(email, strtok(NULL, "/"));
-			char genero = strtok(NULL, "/")[0];	//el género tiene un único carácter
-			
-			
-			char query1[500];
-			sprintf(query1, "SELECT Usuario FROM Jugador WHERE Usuario='%s'", nombre);
-			int result = mysql_query(conn, query1);
-			if(result != 0){
-				sprintf(buff2, "-1");
-			}
-			else{
-				resultado = mysql_store_result (conn);
-				row = mysql_fetch_row (resultado);
-				if (row == NULL){
-					char query2[500];
-					sprintf(query2, "INSERT INTO Jugador (Usuario, Password, Mail, Genero) VALUES ('%s', '%s', '%s', '%c');", nombre, password, email, genero);
-					result = mysql_query(conn, query2);
-					printf("%s\n",query2);
-					if(result != 0){
-						sprintf(buff2, "-1");
-					}else{
-						sprintf(buff2, "1");
-					}
-				}else{
-					sprintf(buff2,"0"); //Usuario o contraseña existentes
-				}
-			}
-			
+		}else if(codigo == 2){
+			signup(buff2);
 		}else if(codigo == 3){
-			char nombre[40];
-			strcpy(nombre, strtok(NULL, "/"));
-			char query[500];
-			sprintf(query, "SELECT Usuario FROM Jugador WHERE Usuario='%s'", nombre);
-			int result = mysql_query(conn, query);
-			if(result != 0){
-				sprintf(buff2, "-1");
-			}
-			else{
-				resultado = mysql_store_result (conn);
-				row = mysql_fetch_row (resultado);
-				if (row == NULL)
-					sprintf(buff2,"0");
-				else
-					sprintf(buff2,"1");
-			}
+			existeUsuario(buff2);
 		}else if(codigo == 4){
-			char construccion[50];
-			strcpy(construccion, strtok(NULL, "/"));
-			char query[500];
-			sprintf(query, "SELECT Partidas.ID, Part_Const.X, Part_Const.Y FROM (Construcciones, Partidas, Part_Const) "
-					"WHERE Construcciones.nombre = '%s' "
-					"AND Construcciones.ID = Part_Const.ID_Const "
-					"AND Partidas.ID = Part_Const.ID_Part;", construccion);
-			int err = mysql_query(conn, query);
-			if(err != 0){
-				sprintf(buff2, "-1");
-			}else{
-				resultado = mysql_store_result (conn);
-				row = mysql_fetch_row(resultado);
-				if(row == NULL){
-					sprintf(buff2, "0");
-				}else{
-					sprintf(buff2, "%s/%s/%s", row[0], row[1], row[2]);
-					row = mysql_fetch_row (resultado);
-					while(row != NULL){
-						sprintf(buff2, "%s/%s/%s/%s", buff2, row[0], row[1], row[2]);
-						row = mysql_fetch_row (resultado);
-					}
-					printf("%s\n", buff2);
-				}
-			}
-			
-		}
-		else if(codigo==5){
-			char nombre[80];
-			token=strtok(NULL,"/");
-			strcpy(nombre,token);
-			char query1[500];
-			sprintf(query1, "SELECT Jug_Part.Dinero FROM (Jugador, Partidas,Jug_Part) WHERE Jugador.Usuario = '%s' AND Jugador.ID = Jug_Part.Jugador AND Partidas.ID = Jug_Part.Partida;", nombre);
-			int result = mysql_query(conn, query1);
-			if(result != 0){
-				sprintf(buff2, "-1");
-			}
-			else{
-				resultado = mysql_store_result (conn);
-				row = mysql_fetch_row (resultado);
-				if (row == NULL)
-					sprintf(buff2,"-2");
-				else{
-					int dtotal=0;
-					while(row!=NULL){
-						dtotal += atoi(row[0]);
-						row = mysql_fetch_row (resultado);
-					}
-					sprintf(buff2,"%d",dtotal);
-				}
-			}
-		}
-		else if(codigo==6){
-			char query[500];
-			sprintf(query, "SELECT Jugador.Usuario, Logros.descripcion FROM (Jugador, Logros, Jug_Log) WHERE Jug_Log.logro_obtenido = 'yes' AND Jug_Log.recompensa_obtenida = 'no' AND Jugador.ID = Jug_Log.ID_Jugadores AND Logros.ID = Jug_Log.ID_Logro;");
-			int err = mysql_query(conn, query);
-			if(err != 0){
-				sprintf(buff2, "-1");
-			}else{
-				resultado = mysql_store_result (conn);
-				row = mysql_fetch_row(resultado);
-				if(row == NULL){
-					sprintf(buff2, "0");
-				}else{
-					sprintf(buff2, "%s/%s", row[0], row[1]);
-					row = mysql_fetch_row (resultado);
-					while(row != NULL){
-						sprintf(buff2, "%s/%s/%s", buff2, row[0], row[1]);
-						row = mysql_fetch_row (resultado);
-					}
-					printf("%s\n", buff2);
-				}
-			}
+			consultaBasicaConstrucciones(buff2);
+		}else if(codigo==5){
+			consultaBasicaDinero(buff2);
+		}else if(codigo==6){
+			consultaBasicaLogros(buff2);
 		}
 
 		//imprimeix el buffer al socket i tanca'l
